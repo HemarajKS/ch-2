f8fae1591579fab262e7e1d15a85bc25909d1c10
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-05-25 22:23:58 +0530

    main

--- src/containers/Details/Details.jsx
+++ src/containers/Details/Details.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import classes from './Details.module.scss';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  LineSeries,
+} from 'react-vis';
 
 const Details = () => {
   return (
@@ -9,7 +16,20 @@ const Details = () => {
         <div className={classes.stockSubname}>INFY|LTD</div>
       </div>
 
-      <div className={classes.detailsGraph}></div>
+      <div className={classes.detailsGraph}>
+        <XYPlot width={800} height={200}>
+          <HorizontalGridLines />
+          <LineSeries
+            data={[
+              { x: 1, y: 10 },
+              { x: 2, y: 5 },
+              { x: 3, y: 15 },
+            ]}
+          />
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      </div>
     </div>
   );
 };
